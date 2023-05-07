2a68de5782d9cadbd6b7526bd451881998ac19dc
Sammi Husky <sammi-husky@live.com>
2023-05-07 00:38:53 -0700

    Intitial Commit

---
+++ AsyncRSP/source/rel.cpp
@@ -0,0 +1,41 @@
+#include "css_hooks.h"
+
+namespace Syringe {
+
+    extern "C" {
+    typedef void (*PFN_voidfunc)();
+    __attribute__((section(".ctors"))) extern PFN_voidfunc _ctors[];
+    __attribute__((section(".ctors"))) extern PFN_voidfunc _dtors[];
+
+    void _prolog();
+    void _epilog();
+    void _unresolved();
+    }
+
+    void _prolog()
+    {
+        // Run global constructors
+        PFN_voidfunc* ctor;
+        for (ctor = _ctors; *ctor; ctor++)
+        {
+            (*ctor)();
+        }
+
+        CSSHooks::InstallHooks();
+    }
+
+    void _epilog()
+    {
+        // run the global destructors
+        PFN_voidfunc* dtor;
+        for (dtor = _dtors; *dtor; dtor++)
+        {
+            (*dtor)();
+        }
+    }
+
+    void _unresolved(void)
+    {
+    }
+
+}
---
+++ NetLog/include/net_log.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace NetLog {
+    int Init();
+    void InstallHooks();
+}
